db5a38fd97c1cbf6be93512cf9cbd04f44498e22
Ian Chan <ianchan1108@hotmail.com>
2024-01-03 21:53:21 -0800

    Add displayUpdate and displayScore functions

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let winner = "";
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    winner = "Tie!";
-    console.log("tie");
+    winner = "tie";
   } else if (
     (playerSelection.toLowerCase() === "rock" &&
       computerSelection === "scissors") ||
@@ -15,8 +14,7 @@ function playRound(playerSelection, computerSelection) {
       computerSelection == "paper")
   ) {
     playerScore++;
-    winner = "You won!";
-    console.log(winner);
+    winner = "player";
   } else if (
     (computerSelection === "rock" &&
       playerSelection.toLowerCase() === "scissors") ||
@@ -26,11 +24,84 @@ function playRound(playerSelection, computerSelection) {
       playerSelection.toLowerCase() === "paper")
   ) {
     computerScore++;
-    winner = "Opponent wins!";
-    console.log(winner);
+    winner = "computer";
   }
 }
 
-// let playerSelection = prompt("Rock, Paper, or Scissors?");
-let computerSelection = "rock";
-console.log(playRound(playerSelection, computerSelection));
+function computerChoice() {
+  let randomChoice = Math.floor(Math.random() * 3) + 1;
+  switch (randomChoice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
+
+function gameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
+
+// DOM
+const resultMessage = document.getElementById("result-message");
+const resultScore = document.getElementById("result-score");
+const playerChoiceSelect = document.querySelector("[data-player-choice]");
+const computerChoiceSelect = document.querySelector("[data-opponent-choice]");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("opponent-score");
+const rockButton = document.querySelector("[data-rock]");
+const paperButton = document.querySelector("[data-paper]");
+const scissorsButton = document.querySelector("[data-scissors]");
+
+rockButton.addEventListener("click", () => game("rock"));
+paperButton.addEventListener("click", () => game("paper"));
+scissorsButton.addEventListener("click", () => game("scissors"));
+
+function game(playerSelection) {
+  if (gameOver()) {
+    return;
+  }
+  const computerSelection = computerChoice();
+  playRound(playerSelection, computerSelection);
+  displayUpdate(playerSelection, computerSelection);
+  displayScore();
+}
+
+function displayUpdate(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case "rock":
+      playerChoiceSelect.textContent = "🗿";
+      break;
+    case "paper":
+      playerChoiceSelect.textContent = "🧻";
+      break;
+    case "scissors":
+      playerChoiceSelect.textContent = "✂️";
+      break;
+  }
+  switch (computerSelection) {
+    case "rock":
+      computerChoiceSelect.textContent = "🗿";
+      break;
+    case "paper":
+      computerChoiceSelect.textContent = "🧻";
+      break;
+    case "scissors":
+      computerChoiceSelect.textContent = "✂️";
+      break;
+  }
+}
+
+function displayScore() {
+  if (winner === "tie") {
+    resultScore.textContent = "It's a Tie!";
+  } else if (winner === "player") {
+    resultScore.textContent = "You Won!";
+    playerScoreDisplay.textContent = `Player: ${playerScore}`;
+  } else if (winner === "computer") {
+    resultScore.textContent = "You Lost!";
+    computerScoreDisplay.textContent = `Opponent: ${computerScore}`;
+  }
+}
